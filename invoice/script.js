8402a08c9d6c84b37783aafe598e3a930fae9675
akhilesh.yaduwanshi313918@gmail.com <akhilesh.yaduwanshi313918@gmail.com>
2023-12-31 23:52:12 +0530

    modified updateInvoice

--- invoice/script.js
+++ invoice/script.js
@@ -1,117 +1,141 @@
 let invoiceForm = document.getElementById("invoiceForm");
 let tbody = document.querySelector("tbody");
 let table = document.querySelector("table");
+let customerList = document.getElementById("customerList");
+let customerName = document.getElementById("customerName");
+let AddressNPhone = document.getElementById("Address&Phone");
+let gstNum = document.getElementById("gstNum");
+let invoiceNum = document.getElementById("invoiceNum");
 let rowIndex;
 
-invoiceForm.addEventListener('submit', (e) => {
-	e.preventDefault();
-	
-
-	const formData = new FormData(invoiceForm);
-	// for(item of formData){
-	// 	console.log(item);
-	// }
-
-	invoice.id =  formData.get("invoiceNum");
-	invoice.customerName =  formData.get("customerName");
-	invoice.addressAndPhone =  formData.get("Address&Phone");
-	invoice.gstNum =  formData.get("gstNum");
-	invoice.date =  formData.get("date").toString();
-	
-	let rowLegnth = table.rows.length;
-	console.log("Table length is "+rowLegnth);
-	for(let i=1; i<rowLegnth; i++){
-		//gets cells of current row
-		let oCells = table.rows.item(i).cells;
-		console.log("Total num of cells -> "+oCells.length);
-
-		//gets amount of cells of current row
-		var cellLength = oCells.length;
-		//loops through each cell in current row
-		// for(var j = 1; j < cellLength-1; j++){
-		// 	/* get your cell info here */
-		// 	var cellVal = oCells.item(j).firstChild.value;
-		// 	console.log("j - "+j);
-			console.log("cell data - "+oCells.item(1).getElementsByTagName('input')[0].value);
-			item.productName = oCells.item(1).getElementsByTagName('input')[0].value
-
-			console.log("cell data - "+oCells.item(2).getElementsByTagName('input')[0].value);
-			item.description = oCells.item(2).getElementsByTagName('input')[0].value
-
-			console.log("cell data - "+oCells.item(3).getElementsByTagName('input')[0].value);
-			item.hsncode = oCells.item(3).getElementsByTagName('input')[0].value
-
-			console.log("cell data - "+oCells.item(4).getElementsByTagName('input')[0].value);
-			item.quantity = oCells.item(4).getElementsByTagName('input')[0].value
-
-			console.log("cell data - "+oCells.item(5).getElementsByTagName('input')[0].value);
-			item.price = oCells.item(5).getElementsByTagName('input')[0].value
-
-			console.log("cell data - "+oCells.item(6).getElementsByTagName('input')[0].value);
-			item.total = oCells.item(6).getElementsByTagName('input')[0].value
-
-			// item.push(oCells.item(j).firstChild.value);
-			// if(item.length > 6){
-			// 	item.splice(0,item.length)
-			// }
-		// }
-	// item.productName = formData.get("productName");
-	// item.description = formData.get("description");
-	// item.hsncode = formData.get("hsncode");
-	// item.quantity = formData.get("qty");
-	// item.price = formData.get("rate");
-	// item.total = formData.get("total");
-	invoice.items.push(item);
-	}
-	
-
-	// invoice.items.push(item);
-	
-	console.log("invoice data -> "+ JSON.stringify(invoice));
-	createUser(invoice);
+function getLatestInvoiceId(){
+fetch("http://localhost:3000/invoiceid")
+.then((response) => response.json())
+.then((data) => {
+	let latestInvoiceNum = data[0].id;
+	invoiceNum.value = latestInvoiceNum;
 })
- 
+}
+getLatestInvoiceId();
+
+invoiceForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(invoiceForm);
+  // for(item of formData){
+  // 	console.log(item);
+  // }
+
+  invoice.id = formData.get("invoiceNum");
+  invoice.customerName = formData.get("customerName");
+  invoice.addressAndPhone = formData.get("Address&Phone");
+  invoice.gstNum = formData.get("gstNum");
+  invoice.date = formData.get("date").toString();
+
+  let rowLegnth = table.rows.length;
+  console.log("Table length is " + rowLegnth);
+  for (let i = 1; i < rowLegnth; i++) {
+    //gets cells of current row
+    let oCells = table.rows.item(i).cells;
+    console.log("Total num of cells -> " + oCells.length);
+
+    //gets amount of cells of current row
+    var cellLength = oCells.length;
+    //loops through each cell in current row
+    // for(var j = 1; j < cellLength-1; j++){
+    // 	/* get your cell info here */
+    // 	var cellVal = oCells.item(j).firstChild.value;
+    // 	console.log("j - "+j);
+    console.log(
+      "cell data - " + oCells.item(1).getElementsByTagName("input")[0].value
+    );
+    item.productName = oCells.item(1).getElementsByTagName("input")[0].value;
+
+    console.log(
+      "cell data - " + oCells.item(2).getElementsByTagName("input")[0].value
+    );
+    item.description = oCells.item(2).getElementsByTagName("input")[0].value;
+
+    console.log(
+      "cell data - " + oCells.item(3).getElementsByTagName("input")[0].value
+    );
+    item.hsncode = oCells.item(3).getElementsByTagName("input")[0].value;
+
+    console.log(
+      "cell data - " + oCells.item(4).getElementsByTagName("input")[0].value
+    );
+    item.quantity = oCells.item(4).getElementsByTagName("input")[0].value;
+
+    console.log(
+      "cell data - " + oCells.item(5).getElementsByTagName("input")[0].value
+    );
+    item.price = oCells.item(5).getElementsByTagName("input")[0].value;
+
+    console.log(
+      "cell data - " + oCells.item(6).getElementsByTagName("input")[0].value
+    );
+    item.total = oCells.item(6).getElementsByTagName("input")[0].value;
+
+    // item.push(oCells.item(j).firstChild.value);
+    // if(item.length > 6){
+    // 	item.splice(0,item.length)
+    // }
+    // }
+    // item.productName = formData.get("productName");
+    // item.description = formData.get("description");
+    // item.hsncode = formData.get("hsncode");
+    // item.quantity = formData.get("qty");
+    // item.price = formData.get("rate");
+    // item.total = formData.get("total");
+    invoice.items.push(item);
+  }
+
+  // invoice.items.push(item);
+
+  console.log("invoice data -> " + JSON.stringify(invoice));
+  createUser(invoice);
+});
+
 var invoice = {
-	id : Number,
-	customerName : String,
-	addressAndPhone : String,
-	gstNum: String,
-	date: String,
-	items : []
+  id: Number,
+  customerName: String,
+  addressAndPhone: String,
+  gstNum: String,
+  date: String,
+  items: [],
 };
 
 var item = {
-	productName : String,
-	description: String,
-	hsncode: String,
-	quantity: Number,
-	price: Number,
-	total: Number
+  productName: String,
+  description: String,
+  hsncode: String,
+  quantity: Number,
+  price: Number,
+  total: Number,
 };
 
+const createUser = async (invoice) => {
+  let bodydata = invoice;
 
-const createUser = async(invoice)=> {
-    let bodydata = invoice;
-    
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json',
-        },
-        body: JSON.stringify(bodydata)
-    };
-    const url = 'http://localhost:3000/invoices';
-    const response = await fetch(url, options);
-    const data = await response.json();
-    return data;
-}
-	
-function onAdd(){
-	let rowLegnth = table.rows.length;
-	console.log("Table length is "+rowLegnth);
-	rowIndex = rowLegnth;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(bodydata),
+  };
+  const url = "http://localhost:3000/invoices";
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
 
-	let markup = `<tr>
+function onAdd() {
+  let rowLegnth = table.rows.length;
+  console.log("Table length is " + rowLegnth);
+  rowIndex = rowLegnth;
+
+  let markup = `<tr>
 	<th scope="row">${rowIndex}</th>
 	<td>
 	  <input
@@ -155,79 +179,143 @@ function onAdd(){
 		name="total"
 		type="number"
 		class="form-control text-end"
+		disabled
 	  />
 	</td>
 	<td>
 	  <i class="fa-solid fa-trash"></i>
 	</td>
   </tr>`;
-tbody.insertAdjacentHTML('beforeend',markup);
+  tbody.insertAdjacentHTML("beforeend", markup);
 }
 
-function onDelete(e){
-	if(!e.target.classList.contains("fa-trash")){
-		console.log("inside if");
-		return;
+function onDelete(e) {
+  if (!e.target.classList.contains("fa-trash")) {
+    console.log("inside if");
+    return;
+  }
+  const btn = e.target;
+  btn.closest("tr").remove();
+}
+
+table.addEventListener("click", onDelete);
+
+function calc(e) {
+  let index = e.parentElement.parentElement.rowIndex - 1;
+  console.log(e.parentElement.parentElement.rowIndex);
+  let qty = document.getElementsByName("qty")[index].value;
+  let price = document.getElementsByName("rate")[index].value;
+  let total = qty * price;
+  document.getElementsByName("total")[index].value = total;
+  calcSubTotal();
+}
+
+function calcSubTotal() {
+  let sum = 0;
+  let totals = document.getElementsByName("total");
+  console.log(totals);
+  for (let i = 0; i < totals.length; i++) {
+    sum = +sum + +totals[i].value;
+  }
+  console.log("sum - " + sum);
+  document.getElementById("subtotal").value = sum.toFixed(2);
+  calcCgst();
+  calcSgst();
+  calcIgst();
+  calcNetTotal();
+}
+
+function calcCgst() {
+  //console.log("calcCgst "+e.value);
+  let subtotal = document.getElementById("subtotal").value;
+  let cgstSelect = document.getElementById("cgstSelect").value;
+  document.getElementById("cgst").value = (
+    subtotal *
+    (cgstSelect * 0.01)
+  ).toFixed(2);
+  calcNetTotal();
+}
+
+function calcSgst() {
+  let subtotal = document.getElementById("subtotal").value;
+  let sgstSelect = document.getElementById("sgstSelect").value;
+  document.getElementById("sgst").value = (
+    subtotal *
+    (sgstSelect * 0.01)
+  ).toFixed(2);
+  calcNetTotal();
+}
+
+function calcIgst(e) {
+  let subtotal = document.getElementById("subtotal").value;
+  let igstSelect = document.getElementById("igstSelect").value;
+  document.getElementById("igst").value = (
+    subtotal *
+    (igstSelect * 0.01)
+  ).toFixed(2);
+  calcNetTotal();
+}
+
+function calcNetTotal() {
+  let subtotal = document.getElementById("subtotal").value;
+  let cgst = document.getElementById("cgst").value;
+  let sgst = document.getElementById("sgst").value;
+  let igst = document.getElementById("igst").value;
+  document.getElementById("nettotal").value = Math.floor(
+    +subtotal + +cgst + +sgst + +igst
+  );
+}
+
+function loadCustomer() {
+	customerList.style.display = "block";	
+  fetch("http://localhost:3000/customers")
+    .then((response) => response.json())
+    .then((customers) => showCustomers(customers));
+
+  function showCustomers(customers) {
+    const ul = document.createElement("ul");
+    ul.className = "list-group";
+    for (let customer of customers) {
+      const li = document.createElement("li");
+      li.className = "list-group-item";
+      li.innerText = customer.customerName;
+	  li.onclick = function() { selectCustomer(this); };
+      ul.appendChild(li);
+      customerList.appendChild(ul);
+    }
+  }
+}
+
+customerName.oninput = filterUsers;
+function filterUsers() {
+  const liElements = document.getElementsByClassName("list-group-item");
+  for (let li of liElements) {
+    const currentCustName = li.innerText.toLowerCase();
+    const searchedCustName = customerName.value.toLocaleLowerCase();
+    if (!currentCustName.includes(searchedCustName))
+      li.setAttribute("hidden", true);
+    else li.removeAttribute("hidden");
+  }
+}
+
+function selectCustomer(e){
+	//alert(e.innerText)
+	customerName.value = e.innerText;
+	customerList.style.display = "none";
+	populateAddressGst(customerName.value);
+}
+
+function populateAddressGst(customerName){
+	fetch("http://localhost:3000/customers")
+    .then((response) => response.json())
+    .then((customers) => setCustomerAddAndGst(customerName,customers));
+
+	function setCustomerAddAndGst(customerName, customers){
+		for(let customer of customers){
+			if (customer.customerName === customerName){
+				AddressNPhone.value = customer.addressAndPhone;
+				gstNum.value = customer.gst;
+			}
+		}
 	}
-	const btn = e.target;
-	btn.closest("tr").remove();
-
-}
-
-table.addEventListener("click", onDelete)
-
-function calc(e){
-
-	let index = e.parentElement.parentElement.rowIndex - 1;
-	console.log(e.parentElement.parentElement.rowIndex);
-	let qty = document.getElementsByName("qty")[index].value;
-	let price = document.getElementsByName("rate")[index].value;
-	let total = qty*price;
-	document.getElementsByName("total")[index].value = total;
-	calcSubTotal();
-}
-
-function calcSubTotal(){
-	let sum = 0;
-	let totals = document.getElementsByName("total");
-	console.log(totals);
-	for(let i=0; i<totals.length; i++){
-		sum = +(sum) + +(totals[i].value);
-	}
-	console.log("sum - "+sum)
-	document.getElementById("subtotal").value = (sum).toFixed(2);
-	calcCgst();
-	calcSgst();
-	calcIgst()
-	calcNetTotal();
-}
-
-function calcCgst(){
-	//console.log("calcCgst "+e.value);
-	let subtotal = document.getElementById("subtotal").value;
-	let cgstSelect = document.getElementById("cgstSelect").value;
-	document.getElementById("cgst").value = (subtotal*(cgstSelect*(0.1))).toFixed(2);
-	calcNetTotal();
-}
-
-function calcSgst(){
-	let subtotal = document.getElementById("subtotal").value;
-	let sgstSelect = document.getElementById("sgstSelect").value;
-	document.getElementById("sgst").value = (subtotal*(sgstSelect*(0.1))).toFixed(2);
-	calcNetTotal();
-}
-
-function calcIgst(e){
-	let subtotal = document.getElementById("subtotal").value;
-	let igstSelect = document.getElementById("igstSelect").value;
-	document.getElementById("igst").value = (subtotal*(igstSelect*(0.1))).toFixed(2);
-	calcNetTotal();
-}
-
-function calcNetTotal(){
-	let subtotal = document.getElementById("subtotal").value;
-	let cgst = document.getElementById("cgst").value;
-	let sgst = document.getElementById("sgst").value;
-	let igst = document.getElementById("igst").value;
-	document.getElementById("nettotal").value = Math.floor(+(subtotal) + +(cgst) + +(sgst) + +(igst));
 }
